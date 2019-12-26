c61ce3ee0a341da19919ddd56346132d4ab680bb
ehale <elaine.hale@nrel.gov>
2019-12-26 15:00:40 -0700

    Changing exception names and adding helpers file with ensure_enum function.

--- dsgutils/__init__.py
+++ dsgutils/__init__.py
@@ -2,8 +2,12 @@ from ._version import __version__
 
 class DSGridError(Exception): pass
 
-class DSGridNotImplemented(DSGridError): pass
+class DSGridAttributeError(DSGridError, AttributeError): pass
 
-class DSGridRuntimeError(DSGridError): pass
+class DSGridIndexError(DSGridError, IndexError): pass
 
-class DSGridValueError(DSGridError): pass
+class DSGridNotImplementedError(DSGridError, NotImplementedError): pass
+
+class DSGridRuntimeError(DSGridError, RuntimeError): pass
+
+class DSGridValueError(DSGridError, ValueError): pass
---
+++ dsgutils/helpers.py
@@ -0,0 +1,18 @@
+
+def ensure_enum(cls, val):
+    """
+    Returns the instance of cls that corresponds to val. cls is expected to be 
+    an enum-type class.
+
+    Parameters
+    ----------
+    cls : an enum class
+    val : str or cls object
+
+    Returns
+    -------
+    cls object
+    """
+    if isinstance(val, str):
+        return cls[val]
+    return cls(val)
