5ce1509650b2da7467ca40a48096345679dffa6b
Elaine Hale <elaine.hale@nrel.gov>
2020-10-09 17:39:26 -0600

    Helper functions for accepting arbitrary log levels on the command line

--- dsgutils/__init__.py
+++ dsgutils/__init__.py
@@ -3,6 +3,8 @@ import logging
 
 from ._version import __version__
 
+logger = logging.getLogger(__name__)
+
 
 # ------------------------------------------------------------------------------
 # Exception Classes
@@ -92,3 +94,47 @@ def end_file_log(logfile):
     logfile.close()
     logging.getLogger().removeHandler(logfile)
     
+
+log_levels = {
+    'CRITICAL': logging.CRITICAL,
+    'ERROR': logging.ERROR,
+    'WARN': logging.WARNING,
+    'WARNING': logging.WARNING,
+    'INFO': logging.INFO,
+    'DEBUG': logging.DEBUG
+}
+
+log_level_strs = {
+    logging.CRITICAL: 'CRITICAL',
+    logging.ERROR: 'ERROR',
+    logging.WARNING: 'WARNING',
+    logging.INFO: 'INFO',
+    logging.DEBUG: 'DEBUG'
+}
+
+def get_log_level(option, default_level = logging.WARNING):
+    """
+    Get the logging level from a str option. Useful for passing desired log level
+    in on the command line.
+
+    Parameters
+    ----------
+    option : str
+    default_level : one of the log_levels.values()
+
+    Returns
+    -------
+    one of the log_levels.values()
+    """
+    level = log_levels.get(option.upper())
+    if level is None:
+        logger.warning(f"Unable to parse {option} as a log level. Defaulting to {default_level}.")
+        level = default_level
+    return level
+
+def get_log_level_str(log_level):
+    """
+    Get a str that corresponds to log_level. Useful, e.g., for saving a log 
+    level to json.
+    """
+    return log_level_strs[log_level]
